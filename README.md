c20f87bcd75616c3d69c125cc02caf34b442e615
MarianoSaez <mm.saez@alumno.um.edu.ar>
2021-04-24 23:33:52 -0300

    readme bonito y ejemplo

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-# error_propagation
+# Propagacion de error en funciones
 Proyecto informatico para la materia Analisis Numerico.
 3ro Ing. Informatica.
 Calculo de error propagado en funciones mediante series de Taylor.
+
+## Modo de uso
+A continuacion se encuentra la salida de la ayuda por consola
+```
+usage: func_error_prop.py [-h] -f FILE
+
+optional arguments:
+-h, --help            show this help message and exit
+-f FILE, --file FILE  Archivo de donde leer valores iniciales
+```
+
+El programa utiliza un archivo de texto a modo de entrada, se puede
+modificar el archivo de ejemplo o proveer otro por argumentos
+```
+func_error_prop.py -f example.txt
+```
+A continuacion se deja un instructivo sobre como debe ser el formato
+con lo que se ingresen o cambien valores en el archivo de entrada
+```
+x+y+z+p
+x y z p
+1 2 3 3.1415
+0.01 0.02 0.03 0.00005
+
+# =============== EJEMPLO ===============
+# La primer fila corresponde a la funcion.
+#   En este caso:
+#               f = x + y + z + p
+#
+# La segunda fila corresponde al vector
+# de variables
+#   En este caso:
+#               x, y, z, p
+#
+# La tercera fila corresponde al vector
+# de valores respectivos a cada variable
+#   En este caso:
+#               x = 1
+#               y = 2
+#               z = 3
+#               p = 3.1415
+#
+# La cuarta fila corresponde al vector
+# de errores respectivos a cada variable
+#   En este caso:
+#               x = 1 +\- 0.01
+#               y = 2 +\- 0.02
+#               z = 3 +\- 0.03
+#               p = 3.1415 +\- 0.00005
+```
+## Referencias
+* [Serie de Taylor](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiI4ZOCrJjwAhV2qJUCHZaqCr8QFjAAegQIBBAD&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FSerie_de_Taylor&usg=AOvVaw0ah1zq-7QBvhtILYOqP0dg)
+* [Fuentes de error](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjduMWHrZjwAhW3oJUCHdzsBOMQFjAMegQIIBAD&url=https%3A%2F%2Festadistica-dma.ulpgc.es%2FFCC%2F05-1-Generalidades-Metodos-Numericos.html&usg=AOvVaw2EbXXnetcxdrraatUCh8U4)
+
+## Bibliografia
+* [Métodos numéricos para ingenieros. Quinta edición. Steven C. Chapra. Raymond P. Canale]
--- func_error_prop.py
+++ func_error_prop.py
@@ -1,4 +1,5 @@
-from sympy import *
+from sympy import symbols, parse_expr, diff, sympify
+import argparse as ap
 
 # Propagacion de errores en funciones - 
 # Este programa calcula el error propagado a traves de funciones en las
@@ -6,22 +7,20 @@ from sympy import *
 # La base de este programa se justifica mediante las series de Taylor.
 
 # TODO:
-# -Calculo del valor de la funcion para el vector de valores
 # -Formatear output
-# -Solucionar input
-# -Input de constantes como pi, e, phi, etc...
-# -Latex???
 
-### INPUT ###
-formula = 'x/(y**2) + z*p'
-vars_vector = ['x', 'y', 'z', 'p']
-values_vector = [1, 2, 3, 3.1416]
-error_vector = [0.01, 0.02, 0.03, 0.00005]
+# Castea a float los strings de un array dado (Usa pasaje por referencia)
+#   @array: Array de strings a ser casteado a array de floats
+def str_to_num(array: list):
+    for i in range(len(array)):
+        array[i] = float(array[i])
 
-# params:
-# @array1: Array de valores que seran el primer elemento del par (simbolos)
-# @array2: Array de valores que seran el segundo elemento del par (valor)
-def to_pair_list(array1:list, array2:list):
+
+# Devuelve una lista de tuplas con pares variable:valor que puede ser
+# utilizada en reemplazos de variables por valores
+#   @array1: Array de valores que seran el primer elemento del par (simbolos)
+#   @array2: Array de valores que seran el segundo elemento del par (valor)
+def to_pair_list(array1: list, array2: list):
     if len(array1) != len(array2):
         raise Exception
     result = list()
@@ -29,15 +28,31 @@ def to_pair_list(array1:list, array2:list):
         aux = ( symbols(array1[i]), array2[i] )
         result.append(aux)
     return result
-        
 
-# params:
-# @function: Funcion a evaluar
-# @variables: Vector de variables que introducen error
-# @values: Vector de valores medidos de las variables
-# @errors: Vector de errores con los que se realizaron las mediciones
-def func_error_prop(function:str, variables:list, values:list, errors:list):
+
+# Calcula el valor de la funcion para los valores de variable ingresados
+#   @function: Funcion a evaluar
+#   @variables: Vector de variables que introducen error
+#   @values: Vector de valores medidos de las variables
+def func_value(function: str, variables: list, values: list):
+    var_value_dict = dict(to_pair_list(variables, values))
+    function = parse_expr(function)
+    func_value = function.subs(var_value_dict)
+
+    return func_value
+
+
+# Calcula la cota de error absoluto (error bound) de una variable
+# dependiente dadas sus variables independientes con sus
+# respectivos valores y errores
+#   @function: Funcion a evaluar
+#   @variables: Vector de variables que introducen error
+#   @values: Vector de valores medidos de las variables
+#   @errors: Vector de errores con los que se realizaron las mediciones
+def func_error_prop(function: str, variables: list, values: list, errors: list):
     # TODO: Validacion de args pasados a la funcion
+    str_to_num(values)
+    str_to_num(errors)
     
     partial_derivatives = list()
     pairs = to_pair_list(variables, values) # Lista de pares ordenados
@@ -57,5 +72,36 @@ def func_error_prop(function:str, variables:list, values:list, errors:list):
     return error_bnd
 
 
-### OUTPUT ###
-print(func_error_prop(formula, vars_vector, values_vector, error_vector))
+
+if __name__ == '__main__':
+    ### INPUT ###
+    str_help = 'Modo de uso de archivo [FILE] para realizar input:\n\n'
+    with open('example.txt', 'r') as f:
+        str_help += f.read()
+    parser = ap.ArgumentParser(formatter_class=ap.RawDescriptionHelpFormatter,
+                               epilog=str_help)
+    parser.add_argument('-f',
+                        '--file',
+                        required=True,
+                        help='Archivo de donde leer valores iniciales',
+                        default='example.txt')
+    args = parser.parse_args()
+    
+    with open(f'{args.file}', 'r+') as f:
+        lines = f.readlines()
+        f.close()
+    
+    formula = lines.pop(0)
+    vars_vector = lines.pop(0).split()
+    values_vector = lines.pop(0).split()
+    error_vector = lines.pop(0).split()
+
+    ### OUTPUT ###
+    err_bnd = func_error_prop(formula, vars_vector, values_vector, error_vector)
+    estim_f = func_value(formula, vars_vector, values_vector)
+    print('==================== ERROR PROPAGATION ====================\n')
+    print(f'f{sympify(vars_vector)} = {sympify(formula)}\n')
+    for i in range(len(vars_vector)):
+        print(f'\t{sympify(vars_vector[i])} = {values_vector[i]} +\\- {error_vector[i]}')
+    print('')
+    print(f'=> f = {estim_f} +\\- {err_bnd}\n')
